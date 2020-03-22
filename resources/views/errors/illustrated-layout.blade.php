00be284c7d3447f0ca90c4c16b8920b4760e458d
dalher <donfumero@gmail.com>
2020-03-22 03:44:39 +0000

    Errors are wonderful now too

--- resources/views/errors/illustrated-layout.blade.php
+++ resources/views/errors/illustrated-layout.blade.php
@@ -1,15 +1,14 @@
-
-
-
-        
 @extends('layouts.allwonder')
 
 @section('content')
 
+    <div class="row justify-content-md-center">
 
-    <div class="row">
-        <div class="col-md-6 offset-md-3 border">
-            <img src="{{ asset('img/isometric.png') }}" style="height: 10rem; width: 10rem;">
+        <div class="col-md-auto py-3 px-5" align="center">
+            <img src="{{ asset('img/isometric.png') }}" class="mx-auto" style="height: 10rem; width: 10rem;">
+        </div>
+
+        <div class="col-md-auto py-3 px-5" align="center">
             <h1 class="display-4">@yield('code', __('Oh no'))</h1>
             <h1 class="font-weight-light">@yield('message')</h1>
             <div class="mt-5">
