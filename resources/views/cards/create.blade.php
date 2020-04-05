f31d4b83cdf73ead37e89d59111439e241f09cad
dalher <donfumero@gmail.com>
2020-04-05 19:58:09 +0100

    Qr code almost added

--- resources/views/cards/create.blade.php
+++ resources/views/cards/create.blade.php
@@ -37,6 +37,12 @@
                             <input name="key" type="text" class="form-control" placeholder="{{ __('Node key')}}" required>
                         </div>
                     </div>
+                    <div class="border border-warning">
+                        <video autoplay="true" id="qrVideo"></video>
+                        <canvas id="qrScreenshot" style="display:none;"></canvas>
+                        <div id="qrStatus" class="border border danger p-5"></div>
+                    </div>
+
                     <button type="submit" class="btn btn-primary">{{ __('Create') }}</button>
                     <small class="mt-4 form-text text-muted">{{ __('For your security, the key will be hidden forever') }}</small>
                 </form>
@@ -45,3 +51,92 @@
     </ul>
 
 @endsection
+
+
+
+@push('scripts')
+    <script>
+        // Select the element where video will be
+        var qrVideo             = document.querySelector("#qrVideo");
+        var qrCanvas            = document.querySelector('#qrScreenshot');
+        var qrStatuss           = document.querySelector('#qrStatus');
+        var front = false;
+
+        // Check for webcam support
+        if (navigator.mediaDevices.getUserMedia) {
+
+            // Broadcast the video to the element or show error
+            navigator.mediaDevices.getUserMedia({ 
+                video: {
+                    // width: {
+                    //    min: 1280,
+                    //    ideal: 1920,
+                    //    max: 2560
+                    // },
+                    // height: {
+                    //    min: 720,
+                    //    ideal: 1080,
+                    //    max: 1440
+                    // },
+                    facingMode: (front? "user" : "environment")
+                } 
+            })
+            .then(function (stream) {
+                qrVideo.srcObject = stream;
+            })
+            .catch(function ( error ) {
+                console.log("Something went wrong!", error);
+            });
+        }
+
+        
+
+        qrVideo.onclick = function() {
+            // Place the pic placeholder where the video was
+            qrVideo.style.display = 'none';
+            qrCanvas.style.display = 'block';
+
+            // Draw the pic in the placeholder
+            qrCanvas.width = qrVideo.videoWidth;
+            qrCanvas.height = qrVideo.videoHeight;
+            qrCanvas.getContext('2d').drawImage(qrVideo, 0, 0);
+
+            // Other browsers will fall back to image/png
+            //img.src = canvas.toDataURL('image/webp');
+
+            // Inform the user about the actions
+            qrStatus.innerHTML = 'Sending...';
+
+            // Send the pic to the API
+            // Alternative
+            // https://file.io/?expires=1w
+            // http://api.qrserver.com/v1/read-qr-code/?fileurl=https://file.io/zmFzks
+            qrCanvas.toBlob(function(blob) {
+                const formData = new FormData();
+                formData.append('file', blob, 'filename.png');
+
+                // Post via axios or other transport method
+                fetch('https://api.qrserver.com/v1/read-qr-code/', {
+                method: 'POST',
+                body: formData,
+                })
+                .then(function(response) {
+                    return response.json();
+                }).then(function(myJson) {
+                    console.log(myJson);
+                })
+            });
+        };
+
+        qrCanvas.onclick = function() {
+            qrVideo.style.display = 'block';
+            qrCanvas.style.display = 'none';
+
+            qrStatus.innerHTML = '';
+        };
+
+        // $('#click').append('caca<br>');
+    </script>
+@endpush
+
+
